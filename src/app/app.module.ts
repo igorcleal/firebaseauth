7d54a7ed34027fdce176e64e8742c2f796a57211
unknown <igor.leal@adm-ti-002.bmnet.com.br>
2017-08-29 10:40:25 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,20 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AuthProvider } from '../providers/auth/auth';
+
+
+export const firebaseCredential = {
+  apiKey: "AIzaSyBS4bW770NpTX-fC1GqUSJVoW56XgZwUzM",
+  authDomain: "teste-e4bd3.firebaseapp.com",
+  databaseURL: "https://teste-e4bd3.firebaseio.com",
+  projectId: "teste-e4bd3",
+  storageBucket: "teste-e4bd3.appspot.com",
+  messagingSenderId: "339941043071"
+};
+
 
 @NgModule({
   declarations: [
@@ -14,7 +28,9 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseCredential),
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,7 +40,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    AuthProvider
   ]
 })
-export class AppModule {}
+export class AppModule { }
